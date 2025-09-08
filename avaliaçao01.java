f516ff4c3e3cd094ea4a7e06783244d0782cce3a
gabrielly willeny <willeny.sousa@aluno.edu.pi.gov.br>
2025-09-08 09:12:03 -0300

    Add files via upload

---
+++ avaliaçao01.java
@@ -0,0 +1,34 @@
+public class avaliaçao01 {
+    public static void main(String[] args) {
+        double n1 = 8.5;
+        double n2 = 5.4;
+        double n3 = 3.4;
+        double n4 = 6.5;
+        double n5 = 7.5;
+        
+        double n6 = 9.6;
+        double n7 = 10.0;
+        double n8 = 4.5;
+
+        double mediaBimestre1 = (n1 + n2)/ 2;
+        double mediaBimestre2 = (n3 + n4)/2;
+        double mediaBimestre3 = (n5 + n6)/ 2;
+        double mediaBimestre4 = (n7 + n8)/ 2;
+
+
+        double semestre1 = (mediaBimestre1 + mediaBimestre2)/2;
+        double semestre2 = (mediaBimestre3 + mediaBimestre4)/2;
+
+        System.out.println ("resultado escolar");
+        System.out.printf ("1° bimestre: %.2f/n");
+        System.out.printf ("2° bimestre: %.2f/n");
+        System.out.printf("1° semestre: %.2f/n");
+
+
+
+
+
+
+
+    }
+}
